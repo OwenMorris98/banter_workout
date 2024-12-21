671a4e05185ea27cfb37fdab1e15226a96b0a52f
Owen <opmmorris98@gmail.com>
2024-12-20 20:17:57 -0500

    added documentation to workout store

--- services/workouts/workout-store.ts
+++ services/workouts/workout-store.ts
@@ -1,5 +1,17 @@
 import { createClient } from "@/utils/supabase/server";
-// Function to fetch workout exercises for a user
+
+
+
+/**
+ * Fetches exercises for a given user's workouts.
+ * 
+ * This function queries the database for exercises associated with a user's workouts.
+ * It filters out exercises that are not part of a workout (i.e., WorkoutName is null).
+ * 
+ * @param {string} userId - The ID of the user for whom to fetch exercises.
+ * @returns {Array} An array of exercises with their names and the workouts they belong to.
+ * @throws {Error} Throws an error if the database query fails.
+ */
 export const fetchWorkoutExercises = async (userId: string) => {
   const supabase = await createClient();
   const { data, error } = await supabase
@@ -12,12 +24,25 @@ export const fetchWorkoutExercises = async (userId: string) => {
   }
   return data;
 };
-// Function to fetch workout names for a user
+/**
+ * Function to fetch workout names for a user
+ * @param {string} userId - The ID of the user
+ * @returns {Array} - An array of unique workout names
+ */
 export const fetchWorkoutNames = async (userId: string) => {
   const exercises = await fetchWorkoutExercises(userId);
   return Array.from(new Set(exercises.map((item) => item.WorkoutName))) ?? [];
 };
 
+/**
+ * Fetches a workout by its ID.
+ * 
+ * This function queries the database for a workout with a given ID. It retrieves the workout's details including its exercises.
+ * 
+ * @param {string} slug - The ID of the workout to fetch.
+ * @returns {Object} The workout object with its details and exercises.
+ * @throws {Error} Throws an error if the database query fails.
+ */
 export const fetchWorkoutById = async (slug: string) => {
   const supabase = await createClient();
   const { data, error } = await supabase
@@ -34,6 +59,16 @@ export const fetchWorkoutById = async (slug: string) => {
   return data;
 };
 
+/**
+ * Fetches exercises for a specific workout by its name.
+ * 
+ * This function queries the database for exercises associated with a workout identified by its name. It filters the results to only include exercises for the specified workout and user.
+ * 
+ * @param {string} userId - The ID of the user.
+ * @param {Object} workout - The workout object containing the workout's name.
+ * @returns {Array} An array of exercise names associated with the specified workout.
+ * @throws {Error} Throws an error if the database query fails.
+ */
 export const fetchWorkoutExercisesByName = async (
   userId: string,
   workout: any
@@ -52,6 +87,15 @@ export const fetchWorkoutExercisesByName = async (
   return data;
 };
 
+/**
+ * Fetches all workouts for a specific user.
+ * 
+ * This function queries the database for all workouts associated with a specific user. It filters the results to only include workouts for the specified user and orders them by date in descending order.
+ * 
+ * @param {string} userId - The ID of the user.
+ * @returns {Array} An array of workout objects associated with the specified user.
+ * @throws {Error} Throws an error if the database query fails.
+ */
 export const fetchMyWorkouts = async (userId : string) => {
   const supabase = await createClient()
   const { data, error  } = await supabase
@@ -66,6 +110,15 @@ export const fetchMyWorkouts = async (userId : string) => {
   return data;
 };
 
+/**
+ * Fetches a workout and its associated exercises by the workout's ID.
+ * 
+ * This function queries the database for a specific workout identified by its ID and fetches all exercises associated with that workout. It returns an object containing both the workout data and exercise data.
+ * 
+ * @param {string} id - The ID of the workout.
+ * @returns {Object} An object containing the workout data and exercise data.
+ * @throws {Error} Throws an error if the database query fails.
+ */
 export const fetchMyWorkoutById = async (id : string) => {
   const supabase = await createClient();
   const {
@@ -93,6 +146,14 @@ export const fetchMyWorkoutById = async (id : string) => {
 
 }
 
+/**
+ * Fetches the list of exercises from the database.
+ * 
+ * This function queries the database for all exercises and returns them in a list. It is used to populate the exercise selection for workout creation or editing.
+ * 
+ * @returns {Array} An array of exercise objects.
+ * @throws {Error} Throws an error if the database query fails.
+ */
 export const fetchExerciseList = async () => {
   const supabase = await createClient()
   const { data, error } = await supabase
